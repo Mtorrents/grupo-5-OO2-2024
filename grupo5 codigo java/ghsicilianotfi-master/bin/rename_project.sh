e3c4efb484234c5b5a16a6f912625d5a422aab10
mtorrents <martin.torrents35@gmail.com>
2024-06-19 00:51:30 -0300

    actualizacion

--- grupo5 codigo java/ghsicilianotfi-master/bin/rename_project.sh
+++
@@ -1,3 +0,0 @@
-mv ../ghsicilianotfi/src/main/java/com/unla/ghsicilianotfi ../ghsicilianotfi/src/main/java/com/unla/"$1"
-mv ../ghsicilianotfi/src/test/java/com/unla/ghsicilianotfi ../ghsicilianotfi/src/test/java/com/unla/"$1"
-mv ../ghsicilianotfi ../"$1"
