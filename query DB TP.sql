63eacdf0d42a004f2b232371a5cea2bd65171ef8
mtorrents <martin.torrents35@gmail.com>
2024-06-19 03:45:10 -0300

    actualizacion

--- query DB TP.sql
+++ query DB TP.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
--- Host: localhost    Database: bd-grupo5tp
+-- Host: localhost    Database: grupo5tp
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
@@ -16,33 +16,23 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `InformeProducto`
+-- Table structure for table `informe_producto`
 --
 
-DROP TABLE IF EXISTS `InformeProducto`;
+DROP TABLE IF EXISTS `informe_producto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `InformeProducto` (
-  `idInformeProducto` int NOT NULL,
-  `estado` tinyint(1) NOT NULL,
-  `descripcion` varchar(50) NOT NULL,
-  `precioVenta` int NOT NULL,
-  `cantidad` int NOT NULL,
-  `lote_id` int NOT NULL,
-  PRIMARY KEY (`idInformeProducto`),
-  KEY `lote_id` (`lote_id`),
-  CONSTRAINT `InformeProducto_ibfk_1` FOREIGN KEY (`lote_id`) REFERENCES `LoteStock` (`idLote`)
+CREATE TABLE `informe_producto` (
+  `id_informe` int NOT NULL AUTO_INCREMENT,
+  `detalle` varchar(255) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `responsable` varchar(255) DEFAULT NULL,
+  `id_lote` int DEFAULT NULL,
+  PRIMARY KEY (`id_informe`),
+  KEY `FKcgd2aeok8cdr18veqg6sdfpbp` (`id_lote`),
+  CONSTRAINT `FKcgd2aeok8cdr18veqg6sdfpbp` FOREIGN KEY (`id_lote`) REFERENCES `lote_stock` (`id_lote`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `InformeProducto`
---
-
-LOCK TABLES `InformeProducto` WRITE;
-/*!40000 ALTER TABLE `InformeProducto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `InformeProducto` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,10 +43,10 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-17 17:07:59
+-- Dump completed on 2024-06-19  3:40:38
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
--- Host: localhost    Database: bd-grupo5tp
+-- Host: localhost    Database: grupo5tp
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
@@ -72,34 +62,31 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `LoteStock`
+-- Table structure for table `lote_stock`
 --
 
-DROP TABLE IF EXISTS `LoteStock`;
+DROP TABLE IF EXISTS `lote_stock`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `LoteStock` (
-  `idLote` int NOT NULL,
-  `producto` varchar(50) NOT NULL,
+CREATE TABLE `lote_stock` (
+  `id_lote` int NOT NULL AUTO_INCREMENT,
   `cantidad` int NOT NULL,
-  `fechaDeRecepcion` date NOT NULL,
-  `proveedor` varchar(50) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  `fecha_de_recepcion` date NOT NULL,
   `precio` int NOT NULL,
-  `producto_id` int NOT NULL,
-  PRIMARY KEY (`idLote`),
-  KEY `producto_id` (`producto_id`),
-  CONSTRAINT `LoteStock_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `Producto` (`idProducto`)
+  `proveedor` varchar(60) NOT NULL,
+  `id_informe` int DEFAULT NULL,
+  `id_pedido` int DEFAULT NULL,
+  `id_producto` int DEFAULT NULL,
+  PRIMARY KEY (`id_lote`),
+  KEY `FKdrp2cf6fyugqlx8ujs34ltnq2` (`id_informe`),
+  KEY `FKkcxfu1l8ykjoth3wq4swlfaro` (`id_pedido`),
+  KEY `FKibu8p8wbquaps0xm73508s04f` (`id_producto`),
+  CONSTRAINT `FKdrp2cf6fyugqlx8ujs34ltnq2` FOREIGN KEY (`id_informe`) REFERENCES `informe_producto` (`id_informe`),
+  CONSTRAINT `FKibu8p8wbquaps0xm73508s04f` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
+  CONSTRAINT `FKkcxfu1l8ykjoth3wq4swlfaro` FOREIGN KEY (`id_pedido`) REFERENCES `pedido_aprovisionamiento` (`id_pedido`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `LoteStock`
---
-
-LOCK TABLES `LoteStock` WRITE;
-/*!40000 ALTER TABLE `LoteStock` DISABLE KEYS */;
-/*!40000 ALTER TABLE `LoteStock` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -109,11 +96,9 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-17 17:07:59
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
--- Host: localhost    Database: bd-grupo5tp
+-- Host: localhost    Database: grupo5tp
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
@@ -129,140 +114,23 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `PedidoAprovisionamiento`
+-- Table structure for table `pedido_aprovisionamiento`
 --
 
-DROP TABLE IF EXISTS `PedidoAprovisionamiento`;
+DROP TABLE IF EXISTS `pedido_aprovisionamiento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `PedidoAprovisionamiento` (
-  `idPedidoAprovicionamiento` int NOT NULL,
-  `producto` varchar(50) NOT NULL,
-  `cantidadSolicitada` int NOT NULL,
-  `fechaDeSolicitud` date NOT NULL,
-  `estado` tinyint(1) NOT NULL,
-  `lote_id` int NOT NULL,
-  PRIMARY KEY (`idPedidoAprovicionamiento`),
-  KEY `lote_id` (`lote_id`),
-  CONSTRAINT `PedidoAprovisionamiento_ibfk_1` FOREIGN KEY (`lote_id`) REFERENCES `LoteStock` (`idLote`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `PedidoAprovisionamiento`
---
-
-LOCK TABLES `PedidoAprovisionamiento` WRITE;
-/*!40000 ALTER TABLE `PedidoAprovisionamiento` DISABLE KEYS */;
-/*!40000 ALTER TABLE `PedidoAprovisionamiento` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-17 17:07:59
--- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
---
--- Host: localhost    Database: bd-grupo5tp
--- ------------------------------------------------------
--- Server version	8.0.37
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `persona`
---
-
-DROP TABLE IF EXISTS `persona`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `persona` (
-  `idPersona` int NOT NULL,
-  `apellido` varchar(45) DEFAULT NULL,
-  `nombre` varchar(45) DEFAULT NULL,
-  `tipo_documento` varchar(45) DEFAULT NULL,
-  `pedidoAprovisionamiento_id` int DEFAULT NULL,
-  PRIMARY KEY (`idPersona`),
-  KEY `fk_persona_1_idx` (`pedidoAprovisionamiento_id`),
-  CONSTRAINT `fk_persona_1` FOREIGN KEY (`pedidoAprovisionamiento_id`) REFERENCES `PedidoAprovisionamiento` (`idPedidoAprovicionamiento`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `persona`
---
-
-LOCK TABLES `persona` WRITE;
-/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-17 17:07:59
--- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
---
--- Host: localhost    Database: bd-grupo5tp
--- ------------------------------------------------------
--- Server version	8.0.37
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Producto`
---
-
-DROP TABLE IF EXISTS `Producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Producto` (
-  `idProducto` int NOT NULL,
-  `descripcion` varchar(50) NOT NULL,
-  `precioVenta` int NOT NULL,
+CREATE TABLE `pedido_aprovisionamiento` (
+  `id_pedido` int NOT NULL AUTO_INCREMENT,
   `cantidad` int NOT NULL,
-  PRIMARY KEY (`idProducto`)
+  `descripcion` varchar(255) DEFAULT NULL,
+  `fecha_pedido` date DEFAULT NULL,
+  `id_lote` int DEFAULT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `FK3vhtjdbrhfvl74vk62mnam6lp` (`id_lote`),
+  CONSTRAINT `FK3vhtjdbrhfvl74vk62mnam6lp` FOREIGN KEY (`id_lote`) REFERENCES `lote_stock` (`id_lote`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Producto`
---
-
-LOCK TABLES `Producto` WRITE;
-/*!40000 ALTER TABLE `Producto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Producto` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -273,10 +141,10 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-17 17:07:59
+-- Dump completed on 2024-06-19  3:40:38
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
--- Host: localhost    Database: bd-grupo5tp
+-- Host: localhost    Database: grupo5tp
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
@@ -292,29 +160,26 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Registro`
+-- Table structure for table `producto`
 --
 
-DROP TABLE IF EXISTS `Registro`;
+DROP TABLE IF EXISTS `producto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Registro` (
-  `idRegistro` int NOT NULL,
-  `producto_id` int NOT NULL,
-  PRIMARY KEY (`idRegistro`),
-  KEY `producto_id` (`producto_id`),
-  CONSTRAINT `Registro_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `Producto` (`idProducto`)
+CREATE TABLE `producto` (
+  `id_producto` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `precio` double NOT NULL,
+  `id_lote` int DEFAULT NULL,
+  `id_registro` int DEFAULT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `FK19kiv8qc586u777cfc32lwlx8` (`id_lote`),
+  KEY `FKjb5ejmsmqtu0fjmgkb221u0ye` (`id_registro`),
+  CONSTRAINT `FK19kiv8qc586u777cfc32lwlx8` FOREIGN KEY (`id_lote`) REFERENCES `lote_stock` (`id_lote`),
+  CONSTRAINT `FKjb5ejmsmqtu0fjmgkb221u0ye` FOREIGN KEY (`id_registro`) REFERENCES `registro` (`id_registro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Registro`
---
-
-LOCK TABLES `Registro` WRITE;
-/*!40000 ALTER TABLE `Registro` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Registro` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -325,10 +190,10 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-17 17:08:00
+-- Dump completed on 2024-06-19  3:40:38
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
--- Host: localhost    Database: bd-grupo5tp
+-- Host: localhost    Database: grupo5tp
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
@@ -344,29 +209,22 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `role`
+-- Table structure for table `registro`
 --
 
-DROP TABLE IF EXISTS `role`;
+DROP TABLE IF EXISTS `registro`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `role` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(255) NOT NULL,
-  `enabled` tinyint(1) NOT NULL DEFAULT '1',
-  `nombre` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `registro` (
+  `id_registro` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `id_productos` int DEFAULT NULL,
+  PRIMARY KEY (`id_registro`),
+  KEY `FKrqa5sjkcvi6pow5mtvpjljejy` (`id_productos`),
+  CONSTRAINT `FKrqa5sjkcvi6pow5mtvpjljejy` FOREIGN KEY (`id_productos`) REFERENCES `producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role`
---
-
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -377,10 +235,10 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-17 17:08:00
+-- Dump completed on 2024-06-19  3:40:38
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
--- Host: localhost    Database: bd-grupo5tp
+-- Host: localhost    Database: grupo5tp
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
@@ -403,17 +261,76 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `email` varchar(255) DEFAULT NULL,
-  `enabled` tinyint(1) NOT NULL DEFAULT '1',
-  `password` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `id` bigint NOT NULL,
-  `role_id` bigint NOT NULL,
-  `persona_id` int DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `enabled` bit(1) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `id_registro` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `role_id` (`role_id`),
-  KEY `fk_user_1_idx` (`persona_id`),
-  CONSTRAINT `fk_user_1` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`idPersona`),
-  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`),
+  KEY `FK4xdevs6gunxocej94piexh1wm` (`id_registro`),
+  CONSTRAINT `FK4xdevs6gunxocej94piexh1wm` FOREIGN KEY (`id_registro`) REFERENCES `registro` (`id_registro`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-19  3:40:38
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: grupo5tp
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `role` varchar(100) NOT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKscfpive7aa0o9savdwmxmnaij` (`role`,`user_id`),
+  KEY `FK859n2jvi8ivhui0rl0esws6o` (`user_id`),
+  CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-19  3:40:38
+
+-- Dump completed on 2024-06-19  3:40:38
